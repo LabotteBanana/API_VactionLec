ebdd573347927fe8019b5bdb2cc78b631693d043
최홍석 <chs93114@gmail.com>
2026-01-12 23:43:20 +0900

    지피티...

--- API_VactionLec/Controllers/LoginController.cs
+++ API_VactionLec/Controllers/LoginController.cs
@@ -7,6 +7,72 @@ using DotnetCoreServer.Models;
 
 namespace DotnetCoreServer.Controllers
 {
+    [ApiController]
+    [Route("Login")]
+    public class LoginController : ControllerBase
+    {
+        private readonly IUserDao userDao;
+
+        public LoginController(IUserDao userDao)
+        {
+            this.userDao = userDao;
+        }
+
+        // GET Login/5
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            var user = userDao.GetUser(id);
+            return Ok(user);
+        }
+
+        // POST Login/Facebook
+        [HttpPost("Facebook")]
+        public IActionResult Facebook([FromBody] User requestUser)
+        {
+            if (requestUser == null)
+                return BadRequest("Request body is null");
+
+            if (string.IsNullOrEmpty(requestUser.FacebookID))
+                return BadRequest("FacebookID is required");
+
+            LoginResult result = new LoginResult();
+
+            var user = userDao.FindUserByFUID(requestUser.FacebookID);
+
+            if (user != null && user.UserID > 0)
+            {
+                result.Data = user;
+                result.Message = "OK";
+                result.ResultCode = 1;
+            }
+            else
+            {
+                requestUser.AccessToken = Guid.NewGuid().ToString();
+                userDao.InsertUser(requestUser);
+
+                user = userDao.FindUserByFUID(requestUser.FacebookID);
+
+                result.Data = user;
+                result.Message = "New User";
+                result.ResultCode = 2;
+            }
+
+            return Ok(result);
+        }
+    }
+}
+
+/*
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using DotnetCoreServer.Models;
+
+namespace DotnetCoreServer.Controllers
+{
     [Route("[controller]/[action]")]
     public class LoginController : Controller
     {
@@ -57,3 +123,4 @@ namespace DotnetCoreServer.Controllers
         }
     }
 }
+*/
